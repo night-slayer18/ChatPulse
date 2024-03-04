2b1f8358ef5d1a69961b4fb0ae7dd0d9d983603b
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-03-05 00:46:42 +0530

    added get all messages between users

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -25,7 +25,7 @@ const sendMessage = async (req, res) => {
         if(newMessage){
             conversation.messages.push(newMessage._id);
         }
-        
+
         await Promise.all([conversation.save(), newMessage.save()]);
 
         res.status(201).json(newMessage);
@@ -35,4 +35,25 @@ const sendMessage = async (req, res) => {
     }
 };
 
-module.exports = { sendMessage };
+const getMessage = async (req, res) => {
+    try {
+        const { id:userToChatId } = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: {
+                $all: [senderId, userToChatId]
+            }
+        }).populate("messages");
+
+        if (!conversation) {
+            return res.status(404).json({ error: "Conversation not found" });
+        }
+
+        res.status(200).json(conversation.messages);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+module.exports = { sendMessage, getMessage };
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,7 +1,8 @@
-const { sendMessage } = require('../controllers/message.controller');
+const { sendMessage, getMessage } = require('../controllers/message.controller');
 const protectRoute = require('../middleware/protectRoute');
 
 const router = require('express').Router();
 
+router.get('/:id',protectRoute,getMessage);
 router.post('/send/:id',protectRoute,sendMessage);
 module.exports = router;
