85dd225171760a447deeef629f1acea64b79120c
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-03-04 23:35:25 +0530

    created message and conversation models

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const conversationSchema = new Schema({
+    participants: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        }
+    ],
+    messages: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Message',
+            default: []
+        }
+    ]
+},{timestamps:true});
+
+const Conversation = mongoose.model('Conversation', conversationSchema);
+
+module.exports = Conversation;
