dcfdcbb34170044538e836c2fecadd698fdc3362
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-03-06 20:25:13 +0530

    inital UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import Login from "./pages/login/Login"
+import Home from "./pages/home/Home"
 
 function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login/>
+      <Home/>
     </div>
   )
 }
