39db5251d31c4fe9efe07b3505be6f653566e1ab
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-03-07 18:44:19 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
+// const cors = require('cors');
 
 const connectDB = require('./db/connectDB');
 
@@ -11,9 +11,8 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 
 app.get('/', (req, res) => {
     res.send({ message: 'Hello from server!' });
